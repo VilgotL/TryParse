f26016d6e6a2baeac483bdb463c300010d5f2439
VilgotL <54435285+VilgotL@users.noreply.github.com>
2020-11-17 10:52:50 +0100

    Add files via upload

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int resultat;
+            if (!TryParse(Console.ReadLine(), out resultat))
+                Console.WriteLine("Skriv in en siffra");
+            Console.ReadLine();
+        }
+        public static bool TryParse(string s, out int result)
+        {
+            try
+            {
+                result = int.Parse(s);
+                return true;
+            }
+            catch
+            {
+                result = 0;
+                return false;
+            }
+        }
+    }
+}
